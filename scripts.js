2fad49c2578712b01ef4946db3b470a0a08ea0fc
Igor Larbac0 <igorcabral59@hotmail.com>
2024-08-16 10:53:24 -0300

    SoftWave Teclados

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-alert('Alura midi é um projeto fictício para desenvolvimento pessoal e estudos');
+alert('Alura Teclados é um projeto fictício para desenvolvimento pessoal e estudos');
 
 function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
